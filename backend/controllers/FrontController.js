0ec8a6c820773fab21d819083f13ee6b1d9f8f7e
Gentil Kadson <fernandes.gentilkadsonjac@gmail.com>
2020-12-10 17:04:32 -0300

    Removed get user friends functionality due to an error.

--- backend/controllers/FrontController.js
+++ backend/controllers/FrontController.js
@@ -2,27 +2,27 @@ const mysql = require('../config')
 
 module.exports = {
     getLoggedUser(req, res) {
-        mysql.query('select * from users where id = ?', [global.userId] ,function(err, results) {
+        mysql.query('select * from users where id = ?', [global.userId], function (err, results) {
             res.send(results[0])
         })
     },
 
     getUserLanguages(req, res) {
-        mysql.query('select lang_id from users_languages where user_id = ?', [global.userId], function(err, results) {
-            if(err) {
+        mysql.query('select lang_id from users_languages where user_id = ?', [global.userId], function (err, results) {
+            if (err) {
                 throw err
             } else {
                 const languagesId = results.map((e, i) => {
                     return e.lang_id
-                })  
-                
+                })
+
                 let sources = []
 
                 languagesId.forEach((e, i) => {
-                    mysql.query('select src from languages where id = ?', [e], function(err, results) {
-                        if(err) {
+                    mysql.query('select src from languages where id = ?', [e], function (err, results) {
+                        if (err) {
                             throw err
-                        } else if(languagesId.length === i + 1) {
+                        } else if (languagesId.length === i + 1) {
                             sources.push(results[0])
                             res.send(sources)
                         } else {
@@ -36,18 +36,18 @@ module.exports = {
     },
 
     getNotLoggedUsers(req, res) {
-        mysql.query('select * from users where id != ?', [global.userId], function(err, results) {
-            if(err) {
+        mysql.query('select * from users where id != ?', [global.userId], function (err, results) {
+            if (err) {
                 throw err
             } else {
                 res.send(results)
             }
-        }) 
+        })
     },
 
     getNotLoggedUsersLanguages(req, res) {
-        mysql.query('select lang_id, user_id from users_languages where user_id != ?', [global.userId], function(err, results) {
-            if(err) {
+        mysql.query('select lang_id, user_id from users_languages where user_id != ?', [global.userId], function (err, results) {
+            if (err) {
                 throw err
             } else {
                 const languagesId = results.map((e, i) => {
@@ -55,13 +55,13 @@ module.exports = {
                         lang_id: e.lang_id,
                         user_id: e.user_id
                     }
-                })  
+                })
 
                 const languagesSrc = []
-                
+
                 languagesId.forEach((e, i) => {
-                    mysql.query('select src from languages where id = ?', [e.lang_id], function(err, results) {
-                        if(err) {
+                    mysql.query('select src from languages where id = ?', [e.lang_id], function (err, results) {
+                        if (err) {
                             throw err
                         } else {
                             const final = {
@@ -69,15 +69,15 @@ module.exports = {
                                 lang_src: results[0].src
                             }
 
-                            if(languagesId.length === i + 1) {
+                            if (languagesId.length === i + 1) {
                                 languagesSrc.push(final)
                                 res.send(languagesSrc)
                             } else {
                                 languagesSrc.push(final)
                             }
-                            
-                            
-                            
+
+
+
                         }
                     })
                 })
@@ -88,18 +88,18 @@ module.exports = {
 
     getUserInterests(req, res) {
         mysql.query('select interest_id from users_interests where user_id = ?', [global.userId], (err, results) => {
-            if(err) {
-                throw err 
+            if (err) {
+                throw err
             } else {
                 res.send(results)
-            } 
+            }
 
         })
     },
 
     getUserLanguagesId(req, res) {
-        mysql.query('select lang_id from users_languages where user_id = ?', [global.userId], function(err, results) {
-            if(err) {
+        mysql.query('select lang_id from users_languages where user_id = ?', [global.userId], function (err, results) {
+            if (err) {
                 throw err
             } else {
                 res.send(results)
@@ -108,27 +108,27 @@ module.exports = {
     },
 
     getUserLanguagesName(req, res) {
-        mysql.query('select lang_id from users_languages where user_id = ?', [global.userId], function(err, results) {
-            if(err) {
+        mysql.query('select lang_id from users_languages where user_id = ?', [global.userId], function (err, results) {
+            if (err) {
                 throw err
             } else {
                 const languagesIds = results.map(e => {
                     return e.lang_id
                 })
-                
+
                 const languagesNames = []
 
                 languagesIds.forEach((e, i) => {
-                    mysql.query('select lang from languages where id = ?', [e], function(err, results) {
-                        if(err) {
+                    mysql.query('select lang from languages where id = ?', [e], function (err, results) {
+                        if (err) {
                             throw err
                         } else {
-                            if(i + 1 === languagesIds.length) {
+                            if (i + 1 === languagesIds.length) {
                                 languagesNames.push(results[0].lang)
                                 res.send(languagesNames)
                             } else {
                                 languagesNames.push(results[0].lang)
-                            }  
+                            }
                         }
                     })
                 })
@@ -143,47 +143,47 @@ module.exports = {
 
         filters.forEach((e, i) => {
             mysql.query('select user_id from users_interests where interest_id = ?', [e], (err, results) => {
-                if(err) {
-                    throw err 
+                if (err) {
+                    throw err
                 } else {
                     results.forEach(e => {
-                        if(e.user_id !== global.userId) {
+                        if (e.user_id !== global.userId) {
                             filteredUsersId.add(e.user_id)
                         }
                     })
 
                     Array.from(filteredUsersId).forEach(e => {
-                            filteredUsersIdRes.push({
-                                user_id: e
-                            })
+                        filteredUsersIdRes.push({
+                            user_id: e
+                        })
 
                     })
-                    
-                    if(i === filters.length - 1) {
+
+                    if (i === filters.length - 1) {
                         res.send(filteredUsersIdRes)
                     }
                 }
             })
         })
-        
+
     },
 
     getUserFriends(req, res, next) {
         let friends = []
         mysql.query('select id from is_friends_with where user1_id = ? or user2_id = ?', [global.userId, global.userId], (err, results) => {
             if (err) {
-                throw err 
+                throw err
             } else {
                 const friendshipId = results
 
                 friendshipId.forEach((e, i) => {
                     mysql.query('select user1_id, user2_id from is_friends_with where id = ?', [e], (err, results) => {
                         if (err) {
-                            throw err 
+                            throw err
                         } else {
                             results.forEach((e, i) => {
-                                if(e.user1_id != global.userId) {
-                                    friends.push(e.user1_id) 
+                                if (e.user1_id != global.userId) {
+                                    friends.push(e.user1_id)
                                 } else {
                                     friends.push(e.user2_id)
                                 }
@@ -191,32 +191,19 @@ module.exports = {
 
                             let friendsUserData = []
                             let friendsLanguage = []
-                            if(i == friendshipId.length - 1) {
+                            if (i == friendshipId.length - 1) {
                                 friends.forEach((e, i) => {
                                     mysql.query('select user_name, src from users where id = ?', [e], (err, results) => {
                                         if (err) {
-                                            throw err 
+                                            throw err
                                         } else {
-                                                friendsUserData.push(results[0])
-
-                                            mysql.query('select lang_id from users_languages where user_id = ?', [e], (err, results) => {
-                                                if (err) {
-                                                    throw err 
-                                                } else {
-                                                    if(i == friendshipId.length-1) {
-                                                        friendsLanguage.push(results)
-                                                        res.send([friendsUserData, friendsLanguage])
-                                                    } else {
-                                                        friendsLanguage.push(results)
-                                                    }
-                                                }
-                                            })
+                                            friendsUserData.push(results[0])
                                         }
-                                        
+
 
                                     })
 
-                                    
+
                                 })
                             }
 
--- frontend/js/friends.js
+++ frontend/js/friends.js
@@ -1,8 +1,17 @@
 import { getInfoUsers, getInfoSrcUsersLanguages, getInfoUserFriends } from './fetch.js'
 
 async function updateFriends() {
+    const usersInfo = []
+    const usersLang = []
+
     const userFriends = await getInfoUserFriends()
     console.log(userFriends)
+    
+    
+    
+
+    // $('#amigos').append(`<div friend class="mx-auto col-9 my-4 rounded container-fluid"><div class="row"><div class="rounded-circle bg-dark col-3 my-3 ml-3" friend-icon></div><div class="ml-3 my-3 col-7 overflow-auto" nickname></div></div><div class="row"><form method="GET" class="mt-4"><input type="submit" value="Perfil" formaction="/profile" class="botao-verde py-1 px-3 ml-3 mb-3"><input type="submit" value="Marcar Chat" class="botao-vermelho py-1 px-3 mx-auto"></form></div></div>`)
+    
 }
 
 async function updateScreen() {
@@ -22,10 +31,8 @@ async function updateScreen() {
         $('[idiomas]').append(element)
 
     })
-
-    updateFriends()
 }
 
 
 updateScreen()
-
+updateFriends()
